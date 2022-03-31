4ff27aa9664a6257d69aaa3f04a8329e12444bb9
Bryce <bstrutt96@gmail.com>
2022-03-31 16:44:53 -0500

    Update DamageMonteCarlo.py
    Added critical hit mechanics and fixed the damage bug

--- DamageMonteCarlo.py
+++ DamageMonteCarlo.py
@@ -1,41 +1,55 @@
 import numpy.random as rn
+import statistics
 
-
-def turnDamage():
-    SHORTSWORD_FREE_DAM = 6
-    UNARMED_FREE_DAM = 4
+def turnDamage(enemy_ac):
+    SHORTSWORD_FREE_DAM = 5
+    UNARMED_FREE_DAM = 3
+    SHORTSWORD_HIT_PLUS = 8
+    UNARMED_HIT_PLUS = 6
     damage = 0
-    a = rn.randint(1, 5)
+
+    a = rn.randint(1, 21)
     if a == 1:
         return damage
-    else:
-        damage = a + rn.randint(1, 3) + SHORTSWORD_FREE_DAM
+    elif a == 20:
+      damage = damage + rn.randint(1,7) + rn.randint(1,7) + rn.randint(1, 5) + SHORTSWORD_FREE_DAM
+    elif (a + SHORTSWORD_HIT_PLUS) >= enemy_ac:
+        damage = damage + rn.randint(1,7) + rn.randint(1, 5) + SHORTSWORD_FREE_DAM
 
-    b = rn.randint(1, 5)
+    b = rn.randint(1, 21)
     if b == 1:
         return damage
-    else:
-        damage = damage + b + rn.randint(1, 3) + SHORTSWORD_FREE_DAM
+    elif b == 20:
+      damage = damage + rn.randint(1,7) + rn.randint(1,7) + rn.randint(1, 5) + SHORTSWORD_FREE_DAM
+    elif (b + SHORTSWORD_HIT_PLUS) >= enemy_ac:
+        damage = damage + rn.randint(1,7) + rn.randint(1, 5) + SHORTSWORD_FREE_DAM
 
-    c = rn.randint(1, 5)
+    c = rn.randint(1, 21)
     if c == 1:
         return damage
-    else:
-        damage = damage + c + UNARMED_FREE_DAM
+    elif c == 20:
+      damage = damage + rn.randint(1,7) + rn.randint(1,7) + UNARMED_FREE_DAM  
+    elif (c + UNARMED_HIT_PLUS) >= enemy_ac:
+        damage = damage + rn.randint(1,7) + UNARMED_FREE_DAM
 
-    d = rn.randint(1, 5)
+    d = rn.randint(1, 21)
     if d == 1:
         return damage
-    else:
-        damage = damage + d + UNARMED_FREE_DAM
+    elif d == 20:
+      damage = damage + rn.randint(1,7) + rn.randint(1,7) + UNARMED_FREE_DAM  
+    elif (d + UNARMED_HIT_PLUS) >= enemy_ac:
+        damage = damage + rn.randint(1,7) + UNARMED_FREE_DAM
 
     return damage
 
-def damageSim(x):
-  totalDamage = 0
+def damageSim(x, enemy_ac):
+  totalDamage = []
   for i in range (0, x):
-    totalDamage = totalDamage + turnDamage()
+    totalDamage.append(turnDamage(enemy_ac))
   return totalDamage
 
 
-print(damageSim(10000)/ 10000)
+for i in range(0,26):
+  enemy_ac = i
+  d = damageSim(10000, enemy_ac)
+  print("Damage at AC " + str(i) + ": " + str(statistics.mean(d)))
